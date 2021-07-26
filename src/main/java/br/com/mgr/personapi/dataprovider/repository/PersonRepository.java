731aa7d8f7af1bb31068a824db66f5431c486af5
Mauri <maurigre@gmail.com>
2021-07-26 17:26:01 -0300

    feat() create repository person

---
+++ src/main/java/br/com/mgr/personapi/dataprovider/repository/PersonRepository.java
@@ -0,0 +1,8 @@
+package br.com.mgr.personapi.dataprovider.repository;
+
+
+import br.com.mgr.personapi.dataprovider.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
