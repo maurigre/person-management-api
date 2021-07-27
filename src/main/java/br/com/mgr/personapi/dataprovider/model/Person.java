2ee1ff4a13a2676558c609ba743f8fe5dc5def89
maurigre@gmail.com <7UUjepGDa4yquMN__!@#$4321>
2021-07-27 01:39:25 -0300

    feat() include annotations for tables Person and Phone

--- src/main/java/br/com/mgr/personapi/dataprovider/model/Person.java
+++ src/main/java/br/com/mgr/personapi/dataprovider/model/Person.java
@@ -1,7 +1,7 @@
 package br.com.mgr.personapi.dataprovider.model;
 
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,10 +9,13 @@ import java.util.List;
 @Entity
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
     private String cpf;
     private LocalDate birthDate;
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Phone> phones = new ArrayList<>();
 }
