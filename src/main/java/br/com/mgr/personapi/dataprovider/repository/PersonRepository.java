a21810e18a9090cfc0cb06ef421c7ada0deb6364
maurigre@gmail.com <7UUjepGDa4yquMN__!@#$4321>
2021-07-28 03:01:23 -0300

    feat() created method findByCpf

--- src/main/java/br/com/mgr/personapi/dataprovider/repository/PersonRepository.java
+++ src/main/java/br/com/mgr/personapi/dataprovider/repository/PersonRepository.java
@@ -4,5 +4,9 @@ package br.com.mgr.personapi.dataprovider.repository;
 import br.com.mgr.personapi.dataprovider.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    Optional<Person> findByCpf(String cpf);
 }
