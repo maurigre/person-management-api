d0b1ab1bd05c45ddc4c18947a087bdfe8d5ae601
maurigre@gmail.com <7UUjepGDa4yquMN__!@#$4321>
2021-07-28 02:55:44 -0300

    refactory() change name controller directory

--- src/main/java/br/com/mgr/personapi/controllers/v1/PersonController.java
+++ src/main/java/br/com/mgr/personapi/controllers/v1/PersonController.java
@@ -1,4 +1,4 @@
-package br.com.mgr.personapi.controller.v1;
+package br.com.mgr.personapi.controllers.v1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
