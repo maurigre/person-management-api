9570554176ba13094f4c253eebb909a6953d9837
maurigre@gmail.com <7UUjepGDa4yquMN__!@#$4321>
2021-08-02 02:01:37 -0300

    feat(): create mapper the objects.

---
+++ src/main/java/br/com/mgr/personapi/controller/v1/dto/mapper/PersonDtoMapper.java
@@ -0,0 +1,23 @@
+package br.com.mgr.personapi.controller.v1.dto.mapper;
+
+import br.com.mgr.personapi.controller.v1.dto.person.PersonDto;
+import br.com.mgr.personapi.controller.v1.dto.person.PhoneDto;
+import br.com.mgr.personapi.core.entity.Person;
+import br.com.mgr.personapi.core.entity.PhoneType;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PersonDtoMapper {
+
+    public static PersonDto personToPersonDto(Person person){
+        final List<PhoneDto> phoneDtos = person.getPhones().stream()
+                .map(phone -> new PhoneDto(phone.getType().getDescription(), phone.getNumber()))
+                .collect(Collectors.toList());
+        return PersonDto.builder()
+                .firstName(person.getFirstName())
+                .lastName(person.getLastName())
+                .cpf(person.getCpf())
+                .phones(phoneDtos).build();
+    }
+}
