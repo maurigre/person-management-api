0f86b017a5c620e164b5c08f5181d23e2635faa8
Mauri <maurigre@gmail.com>
2021-08-09 18:15:24 -0300

    test(): add new tests for findAll and findById person

--- src/test/java/br/com/mgr/personapi/controller/v1/PersonControllerTest.java
+++ src/test/java/br/com/mgr/personapi/controller/v1/PersonControllerTest.java
@@ -5,6 +5,9 @@ import br.com.mgr.personapi.controller.v1.dto.person.PersonDto;
 import br.com.mgr.personapi.controller.v1.dto.person.PhoneDto;
 import br.com.mgr.personapi.core.entity.Phone;
 import br.com.mgr.personapi.core.entity.PhoneType;
+import br.com.mgr.personapi.dataprovider.model.PersonEntity;
+import br.com.mgr.personapi.dataprovider.repository.PersonDao;
+import br.com.mgr.personapi.service.person.PersonService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -16,6 +19,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -24,6 +28,7 @@ import org.springframework.http.MediaType;
 import java.awt.*;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
@@ -39,15 +44,18 @@ class PersonControllerTest {
 
     private final String FIRST_NAME = "Alex";
     private final String LAST_NAME = "Medeiros";
-    private final String CPF = "11111111111";
+    private final String CPF = "44444444444";
     private final LocalDate BIRTH_DATE = LocalDate.of(2019, 12, 01);
     private final List<PhoneDto> PHONES =  List.of(new PhoneDto(PhoneType.COMMERCIAL.getDescription(), "16999994444"));
 
+    @Autowired
+    private PersonDao personDao;
 
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        personDao.deleteAll();
     }
 
     @Test
@@ -69,7 +77,72 @@ class PersonControllerTest {
     }
 
     @Test
-    void givenAPersonWhenPostThenReturnListPersons(){
+    void givenListPersonWhenGetThenReturnOk() throws JsonProcessingException {
+
+        RestAssured.given()
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(
+                        getJsonPayLoad(FIRST_NAME, LAST_NAME, CPF, BIRTH_DATE, PHONES )
+                )
+                .when()
+                .post("api/v1/peoples")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.CREATED.value());
+
+        Optional<PersonEntity> byCpf = personDao.findByCpf(CPF);
+        if (byCpf.isPresent()) {
+            System.out.println( "PERSON: " + byCpf.get().getId());
+        }
+
+        RestAssured.given()
+                .contentType(APPLICATION_JSON_VALUE)
+                .when()
+                .get("api/v1/peoples")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.OK.value());
+
+    }
+
+
+    @Test
+    void givenAnIdPersonWhenGetThenReturnOk() throws JsonProcessingException {
+
+        RestAssured.given()
+                .contentType(APPLICATION_JSON_VALUE)
+                .body(
+                        getJsonPayLoad(FIRST_NAME, LAST_NAME, CPF, BIRTH_DATE, PHONES )
+                )
+                .when()
+                .post("api/v1/peoples")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.CREATED.value());
+
+        Optional<PersonEntity> byCpf = personDao.findByCpf(CPF);
+        UUID id = UUID.fromString("abbb1708-f188-4d0f-acba-12a761ad5e93");
+        if (byCpf.isPresent()) {
+            id = byCpf.get().getId();
+        }
+
+        RestAssured.given()
+                .contentType(APPLICATION_JSON_VALUE)
+                .pathParam("id", id)
+                .when()
+                .get("api/v1/peoples/{id}")
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.OK.value());
+
+    }
+
+    @Test
+    void givenListPersonWhenGetThenReturnBadRequest(){
 
         RestAssured.given()
                 .contentType(APPLICATION_JSON_VALUE)
@@ -78,6 +151,20 @@ class PersonControllerTest {
                 .then()
                 .statusCode(HttpStatus.BAD_REQUEST.value())
                 .body("message", equalTo("People list is empty"));
+
+    }
+
+    @Test
+    void givenAnIdPersonWhenGetThenReturnBadRequest(){
+
+        RestAssured.given()
+                .contentType(APPLICATION_JSON_VALUE)
+                .pathParam("id", "abbb1708-f188-4d0f-acba-12a761ad5e93")
+                .when()
+                .get("api/v1/peoples/{id}")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("message", equalTo("Person not found"));
 
     }
 
@@ -92,11 +179,11 @@ class PersonControllerTest {
 
 
         PersonDto dto = PersonDto.builder()
-                .firstName(FIRST_NAME)
-                .lastName(LAST_NAME)
-                .cpf(CPF)
-                .birthDate(BIRTH_DATE)
-                .phones(PHONES).build();
+                .firstName(firstName)
+                .lastName(lastName)
+                .cpf(cpf)
+                .birthDate(birthDate)
+                .phones(phones).build();
 
 
         ObjectMapper mapper = new ObjectMapper();
